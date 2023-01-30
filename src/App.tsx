8bc1262a991db3fb9dadb4e464ea1cf1481febf4
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-01-30 08:58:12 +0400

    Handle add operation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Show} from 'solid-js'
-import {deleteTodoItem, store} from './store'
+import {addTodoItem, deleteTodoItem, store} from './store'
 import {TodoList} from './TodoList'
 
 export function App() {
@@ -7,7 +7,14 @@ export function App() {
     <section class="todoapp">
       <header class="header">
         <h1>todos</h1>
-        <input class="new-todo" placeholder="What needs to be done?" autofocus />
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          autofocus
+          onChange={(e) => {
+            addTodoItem(e.currentTarget.value)
+          }}
+        />
       </header>
       <Show when={store.todos.length}>
         <TodoList todos={store.todos} onDelete={deleteTodoItem} />
--- src/store.tsx
+++ src/store.tsx
@@ -20,3 +20,7 @@ export {store, setStore}
 export function deleteTodoItem(id: number) {
   setStore('todos', (todos) => todos.filter((todo) => todo.id !== id))
 }
+
+export function addTodoItem(title: string) {
+  setStore('todos', (todos) => [...todos, {id: todos.length + 1, completed: false, title}])
+}
